62baacc4bdcf48b48db0111495eb2977350c2c0f
Poupiste <cristowam@hotmail.fr>
2023-07-08 21:17:23 +0200

    Ravalement de façade et sols

--- Assets/Scripts/Scenes/IntroductionScene.cs
+++ Assets/Scripts/Scenes/IntroductionScene.cs
@@ -3,22 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class IntroductionScene : MonoBehaviour
 {
-    public GameObject[] IntroSlides;
-
     public float TimeBeforePressAnyKeyPrompt = 5f;
     public GameObject PressAnyKeyPrompt;
 
-    private int _currentSlideIndex = -1;
     private float _timeElapsed = 0;
 
     private void Awake()
     {
         PressAnyKeyPrompt.SetActive(false);
-        foreach(var slide in IntroSlides)
-        {
-            slide.SetActive(false);
-        }
-        NextSlide();
     }
 
     private void Update()
@@ -28,21 +20,8 @@ public class IntroductionScene : MonoBehaviour
             PressAnyKeyPrompt.SetActive(true);
         if(Input.anyKeyDown)
         {
-            NextSlide();
-        }
-    }
-
-    private void NextSlide()
-    {
-        PressAnyKeyPrompt.SetActive(false);
-        _timeElapsed = 0;
-        if (_currentSlideIndex >= 0)
-            IntroSlides[_currentSlideIndex].SetActive(false);
-        ++_currentSlideIndex;
-        if (_currentSlideIndex < IntroSlides.Length)
-            IntroSlides[_currentSlideIndex].SetActive(true);
-        else
             GoToLevelsScene();
+        }
     }
 
     private void GoToLevelsScene()
