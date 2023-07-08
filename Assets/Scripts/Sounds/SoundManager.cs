933ba792059c710062c6e349aa4571ec8920d9d5
LilCartell <lilian.cartellier@gmail.com>
2023-07-08 17:50:14 +0200

    Added Sound Manager

---
+++ Assets/Scripts/Sounds/SoundManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+	public AudioSource MusicsSource;
+	public AudioSource SoundEffectsSource;
+
+	public static SoundManager Instance { get; private set; }
+
+	private void Awake()
+	{
+		if (Instance != null && Instance != this)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+
+		Instance = this;
+
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	public void PlayMusic(AudioClip musicClip)
+	{
+		MusicsSource.clip = musicClip;
+		MusicsSource.Play();
+	}
+
+	public void PlaySound(AudioClip effectSound)
+	{
+		SoundEffectsSource.clip = effectSound;
+		SoundEffectsSource.Play();
+	}
+}
